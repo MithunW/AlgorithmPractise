9cd3ad3439b2fdd08529575159af2f897920fe34
MithunW <mithunmedia123@gmail.com>
2025-08-01 22:30:57 +0100

    Add initial implementation of flippingBits function and solution class

---
+++ Hackerrank/flippingBits/Solution.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result{
+    
+    public static long flippingBits(long n){
+        double number = Math.pow(2,32)-1;
+        long output = n ^ Math.round(number);
+        return output;
+        
+        
+    }
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = Integer.parseInt(bufferedReader.readLine().trim());
+        try{
+            for (int i = 0; i<q; i++){
+            long n = Long.parseLong(bufferedReader.readLine().trim());
+            long result = Result.flippingBits(n);
+            bufferedWriter.write(String.valueOf(result)+"\n");
+            }
+        } catch (RuntimeException e){
+            throw new RuntimeException(e);
+        }
+
+        bufferedReader.close();
+        bufferedWriter.close();
+
+        
+    }
+}
